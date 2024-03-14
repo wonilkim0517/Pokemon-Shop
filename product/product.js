9bc3eb64fe35fcbc0e37289fa7084fc4219a09bc
lys0611 <dldptmd611@gmail.com>
2024-03-14 19:23:37 +0900

    타임세일 최최종완성 commit

--- product/product.js
+++ product/product.js
@@ -1,53 +1,63 @@
-
 let loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
 let selectedProduct = JSON.parse(localStorage.getItem('selectedProduct'));
-let cartData = JSON.parse(localStorage.getItem('cartKey'))
+let cartData = JSON.parse(localStorage.getItem('cartKey'));
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('product_title').innerText = selectedProduct.product_name;
-    document.getElementById('price').innerText = selectedProduct.price;
+
+    // 할인된 가격이 있는 경우 해당 가격을 표시하고 아닌 경우 원래 가격 표시
+    const productPrice = document.getElementById('price');
+    if (selectedProduct.discount_price !== undefined) {
+        const originalPrice = document.createElement('p');
+        originalPrice.textContent = `${selectedProduct.price}원 `;
+        originalPrice.style.textDecoration = 'line-through';
+
+        productPrice.appendChild(originalPrice);
+
+        const discountPrice = document.createElement('p');
+        discountPrice.textContent = `할인가: ${selectedProduct.discount_price}원`;
+        discountPrice.style.color = 'red';
+        discountPrice.style.fontSize = '1.2em'; // 할인된 가격의 글꼴 크기를 강조
+        discountPrice.style.fontWeight = 'bold'; // 할인된 가격의 글꼴 굵기를 강조
+
+        productPrice.appendChild(discountPrice);
+    } else {
+        // 할인 가격이 설정되지 않은 경우에는 원래 가격만 표시합니다.
+        productPrice.textContent = `가격: ${selectedProduct.price}원`;
+    }
+
     document.getElementById('product_id').innerText = selectedProduct.id;
-    // document.getElementById('final_price').innerText = totalPrice
-    document.getElementById("final_price").textContent = selectedProduct.price + "원";
-    // document.getElementById("default_img").src = selectedProduct.image;
-});
+    document.getElementById("final_price").textContent = selectedProduct.discount_price !== undefined ? selectedProduct.discount_price.toLocaleString() + "원" : (selectedProduct.price.toLocaleString() + "원");
 
-document.addEventListener("DOMContentLoaded", function() {
-    // 로컬 스토리지에서 selectedProduct 가져오기
-    const selectedProduct = JSON.parse(localStorage.getItem('selectedProduct'));
-
-    // 이미지 요소 찾기
+    // 이미지 요소 설정
     const mainImage = document.getElementById('default_img');
-
-    // selectedProduct가 존재하고 이미지 주소가 있는 경우에만 설정
     if (selectedProduct && selectedProduct.image) {
-        // 이미지의 src 속성을 selectedProduct의 이미지 주소로 설정
-        document.getElementById('main_img').src = "../main/" + selectedProduct.image;
-        document.getElementById('default_img').src = "../main/" + selectedProduct.image;
+        mainImage.src = "../main/" + selectedProduct.image;
 
-        for (var i = 0; i < 11; i++) {
-            var imageId = 'image' + i;
-            var imageDetailId = 'image_detail' + i;
-            var newSrc = (i < 9 ? '0' : '') + (i + 1) + '.png';
-            var imgElement = document.getElementById(imageId);
-            var detailImgElement = document.getElementById(imageDetailId);
+        // 추가 이미지 설정
+        for (let i = 0; i < 11; i++) {
+            const imageId = 'image' + i;
+            const imageDetailId = 'image_detail' + i;
+            const newSrc = (i < 9 ? '0' : '') + (i + 1) + '.png';
+            const imgElement = document.getElementById(imageId);
+            const detailImgElement = document.getElementById(imageDetailId);
             if (imgElement) {
-                var newImageUrl = "../main/" + selectedProduct.image.replace(/\d+\.png/, newSrc);
-                imgElement.onload = function() {
+                const newImageUrl = "../main/" + selectedProduct.image.replace(/\d+\.png/, newSrc);
+                imgElement.onload = function () {
                     console.log("이미지 로드 성공:", this.src);
                 };
-                imgElement.onerror = function() {
+                imgElement.onerror = function () {
                     console.log("이미지 로드 실패:", this.src);
                     this.style.display = "none"; // 이미지를 숨깁니다.
                 };
                 imgElement.src = newImageUrl;
             }
             if (detailImgElement) {
-                var newImageUrl2 = "../main/" + selectedProduct.image.replace(/\d+\.png/, newSrc);
-                detailImgElement.onload = function() {
+                const newImageUrl2 = "../main/" + selectedProduct.image.replace(/\d+\.png/, newSrc);
+                detailImgElement.onload = function () {
                     console.log("이미지 로드 성공:", this.src);
                 };
-                detailImgElement.onerror = function() {
+                detailImgElement.onerror = function () {
                     console.log("이미지 로드 실패:", this.src);
                     this.style.display = "none"; // 이미지를 숨깁니다.
                 };
@@ -55,21 +65,17 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         }
     }
-    // 나머지 초기화 또는 다른 작업 수행 가능
+
+    // 초기화 또는 다른 작업 수행 가능
 });
 
-
-
-
 // - 누르면 수량 감소
-document.querySelector('.minus').addEventListener('click', function(event) {
+document.querySelector('.minus').addEventListener('click', function (event) {
     event.preventDefault(); // 링크 페이지 이동 방지
 
-    // 현재 결과 값 가져오기
     let resultElement = document.getElementById('result');
     let result = parseInt(resultElement.textContent);
 
-    // 결과 값이 1보다 클 때만 감소
     if (result > 1) {
         result--;
         resultElement.textContent = result;
@@ -78,14 +84,12 @@ document.querySelector('.minus').addEventListener('click', function(event) {
 });
 
 // + 누르면 수량 증가
-document.querySelector('.plus').addEventListener('click', function(event) {
+document.querySelector('.plus').addEventListener('click', function (event) {
     event.preventDefault(); // 링크의 기본 동작 방지
 
-    // 현재 결과 값 가져오기
     let resultElement = document.getElementById('result');
     let result = parseInt(resultElement.textContent);
 
-    // 결과 값 증가
     result++;
     resultElement.textContent = result;
     updatePrice();
@@ -93,13 +97,12 @@ document.querySelector('.plus').addEventListener('click', function(event) {
 
 function updatePrice() {
     let quantity = parseInt(document.getElementById("result").textContent);
-    // let priceText = document.getElementById("price").textContent;
-    let price = selectedProduct.price;
+    let price = selectedProduct.discount_price !== undefined ? selectedProduct.discount_price : selectedProduct.price;
     let totalPrice = quantity * price;
     document.getElementById("final_price").textContent = totalPrice.toLocaleString() + "원";
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("default_img").classList.add("selected");
 });
 
@@ -107,50 +110,38 @@ function changeMainImage(clickedImage, newSrc) {
     let mainImage = document.getElementById("main_img");
     let mainImageSrc = mainImage.src;
 
-    // 메인 이미지의 숫자 부분을 클릭한 이미지의 숫자 부분으로 교체
     let newMainImageSrc = mainImageSrc.replace(/\d+\.png/, newSrc + ".png");
     mainImage.src = newMainImageSrc;
 
-    // 모든 이미지의 선택 클래스를 제거
     let allImages = document.querySelectorAll('.img_detail img');
-    allImages.forEach(function(image) {
+    allImages.forEach(function (image) {
         image.classList.remove('selected');
     });
 
-    // 클릭한 이미지에 선택 클래스 추가
     clickedImage.classList.add('selected');
 }
 
-console.log(loggedInUser.id);
-console.log(cartData.user_id)
-console.log(selectedProduct.id)
-console.log(cartData.product_id)
-
 function addNewProductToCart() {
-    // 로그인 확인
     if (!loggedInUser || !loggedInUser.id) {
         alert("로그인이 필요합니다")
         window.location.href = "../login/login-page.html";
         return;
     }
-    // 이미 있는 상품인지 확인
+
     let existingProductIndex = cartData.findIndex(item => item.user_id === loggedInUser.id && item.product_id === selectedProduct.id);
 
     if (existingProductIndex !== -1) {
-        // 이미 있는 상품이면 해당 상품의 cart_quantity를 증가시킴
         cartData[existingProductIndex].cart_quantity += parseInt(document.getElementById("result").textContent);
         localStorage.setItem('cartKey', JSON.stringify(cartData));
         console.log(cartData);
     } else {
-        // 존재하지 않는 상품이면 새로운 상품을 추가
         let productQuantity = parseInt(document.getElementById("result").textContent);
-        // 예시: 새로운 상품 추가
         let newProduct = {
-            "id": cartData.length + 1, // 새로운 상품의 고유 식별자
-            "user_id": loggedInUser.id, // 사용자 ID
-            "product_id": selectedProduct.id, // 상품 ID
-            "cart_quantity": productQuantity, // 장바구니에 담은 상품 수량
-            "is_ordered": 0 // 주문 여부 (0: 주문 전)
+            "id": cartData.length + 1,
+            "user_id": loggedInUser.id,
+            "product_id": selectedProduct.id,
+            "cart_quantity": productQuantity,
+            "is_ordered": 0
         };
 
         cartData.push(newProduct);
@@ -160,6 +151,3 @@ function addNewProductToCart() {
         window.location.href = "../cart/cart.html"
     }
 }
-// addNewProductToCart(cartData);
-
-
